385eb2ed00900eadc99090a2140e47ec65d640bc
mapkwon <kounji02@gmail.com>
2024-09-20 15:47:07 +0900

    0920_3

--- hana/js/ui.front.js
+++ hana/js/ui.front.js
@@ -1,5 +1,35 @@
 //실행함수
 $(window).on('load',function(){
-    
+    fullMenu();
 });
 
+function fullMenu(){
+    $('.header .btn_menu').on('click',function(){
+        if(!$(this).hasClass('active')){
+            $(this).addClass('active');
+            $('.full_menu').addClass('active');
+            $('body').addClass('overflow');
+            $('.header').addClass('blendMode');
+        }
+    });
+
+    $('.full_menu').find('.btn_close').on('click',function(){
+        $('.header .btn_menu').removeClass('active');
+        $('.full_menu').removeClass('active');
+        $('body').removeClass('overflow');
+        $('.header').removeClass('blendMode');
+    });
+
+}
+/*
+function getTextColorByBackgroundColor(hexColor) {
+    const c = hexColor.substring(1)      // 색상 앞의 # 제거
+    const rgb = parseInt(c, 16)   // rrggbb를 10진수로 변환
+    const r = (rgb >> 16) & 0xff  // red 추출
+    const g = (rgb >>  8) & 0xff  // green 추출
+    const b = (rgb >>  0) & 0xff  // blue 추출
+    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709
+    // 색상 선택
+    return luma < 127.5 ? "white" : "black" // 글자색이
+}
+    */
