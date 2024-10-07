001f115321f893a52c0d94f4a5f442a75e91f3de
mapkwon <kounji02@gmail.com>
2024-10-07 14:18:36 +0900

    1007_01

--- hana/js/ui.front.js
+++ hana/js/ui.front.js
@@ -1,6 +1,7 @@
 //실행함수
 $(window).on('load',function(){
     fullMenu();
+    realTimeClock();
 });
 
 function fullMenu(){
@@ -21,6 +22,47 @@ function fullMenu(){
     });
 
 }
+
+function realTimeClock() {
+    var today = new Date();
+
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var pmHours = ('0' + (hours - 12)).slice(-2);
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+    
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+
+    var dateString = year + '/' + month  + '/' + day;
+    var timeString = hours + ':' + minutes  + ':' + seconds;
+    var pmTimeString = pmHours + ':' + minutes  + ':' + seconds;
+
+   // console.log(today,hours,minutes,seconds, year, month, day, pmHours, dateString, timeString)
+
+    if (hours > 12) {
+        $('.visual_sec .profile .clock .mid').text('오후');
+        $('.visual_sec .profile .clock .date').text(dateString);
+        //$('.visual_sec .profile .clock .time').text(pmTimeString);
+        $('.visual_sec .profile .clock .time .pmHours').text(pmHours)
+        $('.visual_sec .profile .clock .time .minutes').text(minutes)
+        $('.visual_sec .profile .clock .time .seconds').text(seconds)
+    } else {
+        $('.visual_sec .profile .clock .mid').text('오전');
+        $('.visual_sec .profile .clock .date').text(dateString);
+        //$('.visual_sec .profile .clock .time').text(timeString);
+        $('.visual_sec .profile .clock .time .hours').text(hours)
+        $('.visual_sec .profile .clock .time .minutes').text(minutes)
+        $('.visual_sec .profile .clock .time .seconds').text(seconds)
+    }	
+    //real time
+    colckAct();
+}
+function colckAct() {
+  setInterval(realTimeClock, 1000);
+}
+
 /*
 function getTextColorByBackgroundColor(hexColor) {
     const c = hexColor.substring(1)      // 색상 앞의 # 제거
@@ -35,53 +77,52 @@ function getTextColorByBackgroundColor(hexColor) {
     */
 $(document).ready(function(){
     setTimeout(function() {
-      $(".loading").fadeOut();
+        $(".loading").fadeOut();
     }, 3500);
-  
-    //menu
-    $(".menu").click(function(){
-      $(".header").toggleClass("open");
-      $(".menu").toggleClass("open");
-      $(".nav-wrap").fadeToggle();
-  
-      if($(".header").is(".open")) {
+
+//menu
+$(".menu").click(function(){
+    $(".header").toggleClass("open");
+    $(".menu").toggleClass("open");
+    $(".nav-wrap").fadeToggle();
+
+    if($(".header").is(".open")) {
         $("a.footer-more").addClass("white")
-      } else {
+    } else {
         $("a.footer-more").removeClass("white")
-      }
-    })
-  
-    $(".blank").click(function(e){
-      e.preventDefault();
-      $(".alert").addClass("on")
-  
-      $(".close").click(function(){
+    }
+})
+
+$(".blank").click(function(e){
+    e.preventDefault();
+    $(".alert").addClass("on")
+
+    $(".close").click(function(){
         $(".alert").removeClass("on")
-      })
     })
-  })
-  
-  $(document).mousemove(function(e) {
+    })
+})
+//cursor
+$(document).mousemove(function(e) {
     $(".cursor").eq(0).css({
-      left: e.pageX,
-      top: e.pageY,
-    });
-  
-    setTimeout(function() {
-      $(".cursor").eq(1).css({
         left: e.pageX,
         top: e.pageY,
-      });
-    }, 120);
-  
-    $("a:not('.work-box li a'), .menu").on({
-      mouseenter: function () { 
-        $(".cursor").addClass("big"); 
-      },
-      mouseleave: function () { 
-        $(".cursor").removeClass("big"); 
-      }
     });
-  })
-  
-  
+
+    setTimeout(function() {
+        $(".cursor").eq(1).css({
+            left: e.pageX,
+            top: e.pageY,
+        });
+    }, 120);
+
+    $("a:not('.work-box li a'), .menu").on({
+        mouseenter: function () { 
+            $(".cursor").addClass("big"); 
+        },
+        mouseleave: function () { 
+            $(".cursor").removeClass("big"); 
+        }
+    });
+})
+
